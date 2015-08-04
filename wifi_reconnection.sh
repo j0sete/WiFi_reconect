18174e43c964fac2fa11917f45ff7f85aec9ba9a
José Manuel Rodríguez Montes <jose92rm@gmail.com>
2015-08-04 12:51:26 +0200

    Create wifi_reconnection.sh

---
+++ wifi_reconnection.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#: Tittle     : wifi_reconnection.sh
+#: Version    : 1.0
+#: Desciption : Test internet connection, if don't have, it tries to reconnect your Wi-Fi interface
+#: Options    : None
+
+## IP to PING and test internet connection
+SERVER=8.8.8.8
+
+ping -c2 ${SERVER} > /dev/null
+
+if [ $? != 0 ]
+then
+  sudo ifdown --force wlan0 #
+  sudo ifup wlan0
+fi
